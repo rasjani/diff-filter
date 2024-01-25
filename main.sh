c5af625569d94d524cc0642fecd8460d692ca7a6
Jani Mikkonen <jani.mikkonen@gmail.com>
2024-01-25 14:00:03 +0200

    fixed all shellcheck warnings

--- main.sh
+++ main.sh
@@ -4,13 +4,14 @@ INPUT_BASE_DIR=$1
 INPUT_DIFF_FILTER=$2
 
 if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
-  BASE_REF=$(git rev-parse origin/${GITHUB_BASE_REF})
+  BASE_REF=$(git rev-parse origin/"${GITHUB_BASE_REF}")
   HEAD_REF=$(git rev-parse HEAD)
 elif  [ "${GITHUB_EVENT_NAME}" == "push" ]; then
   BASE_REF=HEAD
   HEAD_REF="HEAD^"
 else
-  echo "TheProjectAurora/diff-filter action supports only 'pull_request' and 'commit' events." >> ${GITHUB_OUTPUT}
+  echo "${GITHUB_ACTION} action supports only 'pull_request' and 'commit' events."
+  # TODO: Should exit here with error ?
   BASE_REF=HEAD
   HEAD_REF=HEAD
 fi
@@ -20,10 +21,10 @@ AOUTPUT_TMP=$(
     --no-pager \
     diff \
     --name-only \
-    --diff-filter=${INPUT_DIFF_FILTER} \
-    ${BASE_REF} \
-    ${HEAD_REF} | \
-  grep ^${INPUT_BASE_DIR} | \
+    --diff-filter="${INPUT_DIFF_FILTER}" \
+    "${BASE_REF}" \
+    "${HEAD_REF}" | \
+  grep ^"${INPUT_BASE_DIR}" | \
   xargs -n 1 dirname | \
   awk -F/ '{print $2}' | \
   sort | \
@@ -33,4 +34,4 @@ AOUTPUT_TMP=$(
   tr -d "\ \n\r"
 )
 
-echo "dirs=${AOUTPUT_TMP}">>${GITHUB_OUTPUT}
+echo "dirs=${AOUTPUT_TMP}">>"${GITHUB_OUTPUT}"
